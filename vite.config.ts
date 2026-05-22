7072e9276de60d8c80fd90b5cae28d17b62713db
uausmani <43364908+uausmani@users.noreply.github.com>
2026-05-22 18:35:49 +0500

    Update vite.config.ts
    updated base setting and repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/rvlanding-ee3a1a99/",
   server: {
     host: "::",
     port: 8080,
